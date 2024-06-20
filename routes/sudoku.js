0c053d246833931d923db98c986ca3f30ed76b11
Ole Natlandsmyr <ole.natlandsmyr@genus.no>
2024-06-20 16:24:22 +0200

    feat: Allow passing difficulty

--- routes/sudoku.js
+++ routes/sudoku.js
@@ -3,8 +3,23 @@ var sudoku = require('sudoku-gen')
 var router = express.Router()
 
 /* GET sudoku. */
-router.get('/', function (req, res, next) {
-	res.send(sudoku.getSudoku('medium'))
+router.get('/:difficulty', function (req, res, next) {
+	switch (req.params.difficulty) {
+		case '0':
+			res.send(sudoku.getSudoku('easy'))
+			break
+		case '1':
+			res.send(sudoku.getSudoku('medium'))
+			break
+		case '2':
+			res.send(sudoku.getSudoku('hard'))
+			break
+		case '3':
+			res.send(sudoku.getSudoku('expert'))
+			break
+		default:
+			res.send(sudoku.getSudoku('medium'))
+	}
 })
 
 module.exports = router
